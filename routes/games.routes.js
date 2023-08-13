cea1e5adec9c4e0e1997b7ed2d2faac8580bec9c
Isabella Paiva <isabellanpaiva@gmail.com>
2023-08-13 13:01:59 +0200

    version1.2 api connected

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+const gamesAPI = require('../services/games.service')
+
 router.get("/", (req, res, next) => {
-    res.render("games/games-list")
+
+    gamesAPI
+        .getAllGames()
+        //.then(response => res.send(response.data))
+        .then(response => res.render("games/games-list", { games: response.data.games }))
+        .catch(err => next(err))
 })
 
 module.exports = router

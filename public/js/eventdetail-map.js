b72458efdffaf67af2b7486303996fe1f0be61cb
nacho <nachosanson6@gmail.com>
2023-08-17 20:57:49 +0200

    final

---
+++ public/js/eventdetail-map.js
@@ -0,0 +1,20 @@
+const lat = document.querySelector('#lat')
+const lng = document.querySelector('#lng')
+const initialCoords = { lat: Number(lat.value), lng: Number(lng.value) }
+let eventMap
+function initMap() {
+    renderMap()
+    printMarkers()
+}
+function renderMap() {
+    eventMap = new google.maps.Map(
+        document.querySelector('#map'),
+        { zoom: 15, center: initialCoords }
+    )
+}
+function printMarkers() {
+    new google.maps.Marker({
+        position: initialCoords,
+        map: eventMap,
+    })
+}
